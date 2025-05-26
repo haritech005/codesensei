59525c182169867025b1fc7556d1d5d44abfa058
hariharanj-01 <hariharanj@lumel.com>
2025-05-26 16:26:50 +0530

    routes added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <>
+    <div className='text-2xl underline'>Welcome to My SAAS</div>
+    <Button>Hello moto</Button>
+    </>
+    
   )
 }
 
