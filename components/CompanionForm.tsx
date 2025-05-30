bca543716889f57f3628386f8250d3d5f114e0ed
hariharanj-01 <hariharanj@lumel.com>
2025-05-30 10:59:34 +0530

    Supabase connection Implement

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -22,6 +22,8 @@ import {
 import { Input } from "@/components/ui/input"
 import { subjects } from "@/constants"
 import { Textarea } from "./ui/textarea"
+import { createCompanion } from "@/lib/actions/companions.actions"
+import { redirect } from "next/navigation"
 
 
 const formSchema = z.object({
@@ -49,7 +51,14 @@ const CompanionForm = () => {
     })
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        const companion = await createCompanion(values);
+        if (companion) {
+            redirect(`/companions/${companion.id}`)
+        }
+        else {
+            console.log("Failed to create a companion");
+            redirect("/")
+        }
     }
 
     return (
